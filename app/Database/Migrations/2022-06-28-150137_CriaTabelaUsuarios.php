afec3e6c16b5efe4182faf4dcabc693474ac32c4
MickaelFronza <mickael.fronza@gmail.com>
2022-06-28 12:25:32 -0300

    Criando a Migração do banco de dados (Migration) e (rollback)

---
+++ app/Database/Migrations/2022-06-28-150137_CriaTabelaUsuarios.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CriaTabelaUsuarios extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nome' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'telefone' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id', true);
+
+        $this->forge->createTable('usuarios');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('usuarios');
+    }
+}
