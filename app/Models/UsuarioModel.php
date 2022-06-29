1bc4dd597460296bbb60f383d0072b06b422d7df
MickaelFronza <mickael.fronza@gmail.com>
2022-06-28 22:46:33 -0300

    Correção Linha Desconhecida Model

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class UsuarioModel extends Model
-{
+{ 
+    protected $useTimestamps        = true;
+    protected $createdField         = 'criado_em'; 
+    protected $updatedField         = 'atualizado_em'; 
+    protected $deletedField         = 'deletado_em';
     protected $table            = 'usuarios';
     protected $returnType       = 'object';
     protected $useSoftDeletes   = true;
